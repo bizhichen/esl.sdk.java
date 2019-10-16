f5d37f72d2bf8103235deec20caa73d763b69eda
zbi <zhichen.bi@esignlive.com>
2019-10-16 16:16:17 -0400

    PB-32034 Allow customizing New Signer Experience logos via SDK

---
+++ sdk/src/test/java/com/silanis/esl/sdk/builder/SigningLogoBuilderTest.java
@@ -0,0 +1,23 @@
+package com.silanis.esl.sdk.builder;
+
+import com.silanis.esl.sdk.SigningLogo;
+import org.junit.Test;
+
+import java.util.Locale;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class SigningLogoBuilderTest {
+    @Test
+    public void withSpecifiedValues() {
+        SigningLogoBuilder signingLogoBuilder = SigningLogoBuilder.newSigningLogo()
+                .withLanguage(new Locale("en"))
+                .withImage("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEUAAABnCA");
+
+        SigningLogo result = signingLogoBuilder.build();
+
+        assertThat( "language was not set correctly", result.getLanguage(), is(Locale.ENGLISH ));
+        assertThat( "image was not set correctly", result.getImage(), is("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEUAAABnCA" ) );
+    }
+}
